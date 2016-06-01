84b7a548fcfcd8a4229985b26827320d295cbbdd
Andrzej Kuriata <andrzej.kuriata@intel.com>
2016-06-01 09:55:08 +0000

    creating task after plugins are loaded

--- src/snap/start_snap/start_snap.go
+++ src/snap/start_snap/start_snap.go
@@ -5,8 +5,20 @@ import "os/exec"
 import "net/http"
 import "sync"
 import "time"
+import "io/ioutil"
+import "encoding/json"
+import "strconv"
+
+type Plugins struct {
+        Body Body `json:"body"`
+}
+
+type Body struct {
+        LoadedPluigns []interface{} `json:"loaded_plugins"`
+}
 
 func main() {
+        plugins := Plugins{}
         var wg sync.WaitGroup
         wg.Add(2)
         go exec.Command(os.Getenv("SNAPD_BIN"), "-t", "0", "-a", os.Getenv("PLUGINS_AUTOLOAD_DIR")).Run()
@@ -19,10 +31,21 @@ func main() {
                                 continue
                         }
                         if resp.StatusCode == 200 {
+                                body, err := ioutil.ReadAll(resp.Body)
+                                defer resp.Body.Close()
+                                if err != nil {
+                                        return
+                                }
+                                json.Unmarshal(body, &plugins)
+                                numPlugins, _ := strconv.Atoi(os.Getenv("PLUGINS_TO_LOAD"))
+                                if len(plugins.Body.LoadedPluigns) < numPlugins {
+                                        time.Sleep(time.Second)
+                                        continue
+                                }
                                 exec.Command(os.Getenv("SNAPCTL_BIN"), "task", "create", "-t", os.Getenv("TASK_AUTOLOAD_FILE")).Run()
                                 return
                         }
-                        break
+                        continue
                 }
         }()
         wg.Wait()
